393bf2092dc50b74756dff12f095b5360d30934a
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2022-02-15 22:46:24 +0900

    エントリーで入力する

--- chapter.4/cmd/main.go
+++ chapter.4/cmd/main.go
@@ -8,18 +8,28 @@ import (
 )
 
 func main() {
-	c := 0
 	a := app.New()
 
 	w := a.NewWindow("Hello")
 	l := widget.NewLabel("Hello Fyne!")
+	e := widget.NewEntry()
+	e.SetText("0")
 	w.SetContent(widget.NewVBox(
 		l,
+		e,
 		widget.NewButton("Click me!", func() {
-			c++
-			l.SetText("count: " + strconv.Itoa(c))
+			n, _ := strconv.Atoi(e.Text)
+			l.SetText("Total: " + strconv.Itoa(total(n)))
 		}),
 	))
 
 	w.ShowAndRun()
+}
+
+func total(n int) int {
+	var t int
+	for i := 1; i <= n; i++ {
+		t += i
+	}
+	return t
 }
