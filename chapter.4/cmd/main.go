297a7cdd02dca444787e94d26596fe55a475f8eb
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2022-02-18 20:54:24 +0900

    電卓

--- chapter.4/cmd/main.go
+++ chapter.4/cmd/main.go
@@ -1,50 +1,132 @@
 package main
 
 import (
+	"strconv"
+
 	"fyne.io/fyne"
 	"fyne.io/fyne/app"
-	"fyne.io/fyne/theme"
+	"fyne.io/fyne/layout"
 	"fyne.io/fyne/widget"
 )
 
-type MyEntry struct {
-	widget.Entry
-	entered func(e *MyEntry)
+type cdata struct {
+	mem int
+	cal string
+	flg bool
 }
 
-func NewMyEntry(f func(e *MyEntry)) *MyEntry {
-	e := &MyEntry{}
-	e.ExtendBaseWidget((e))
-	e.entered = f
-	return e
+func createNumButtons(f func(v int)) *fyne.Container {
+	c := fyne.NewContainerWithLayout(
+		layout.NewGridLayout(3),
+		widget.NewButton(strconv.Itoa(7), func() { f(7) }),
+		widget.NewButton(strconv.Itoa(8), func() { f(8) }),
+		widget.NewButton(strconv.Itoa(9), func() { f(9) }),
+		widget.NewButton(strconv.Itoa(4), func() { f(4) }),
+		widget.NewButton(strconv.Itoa(5), func() { f(5) }),
+		widget.NewButton(strconv.Itoa(6), func() { f(6) }),
+		widget.NewButton(strconv.Itoa(1), func() { f(1) }),
+		widget.NewButton(strconv.Itoa(2), func() { f(2) }),
+		widget.NewButton(strconv.Itoa(3), func() { f(3) }),
+		widget.NewButton(strconv.Itoa(0), func() { f(0) }),
+	)
+	return c
 }
 
-func (e *MyEntry) KeyDown(key *fyne.KeyEvent) {
-	switch key.Name {
-	case fyne.KeyReturn, fyne.KeyEnter:
-		e.entered(e)
-	default:
-		e.Entry.KeyDown(key)
-	}
+func createClacButtons(f func(c string)) *fyne.Container {
+	c := fyne.NewContainerWithLayout(
+		layout.NewGridLayout(1),
+		widget.NewButton("CL", func() { f("CL") }),
+		widget.NewButton("/", func() { f("/") }),
+		widget.NewButton("*", func() { f("*") }),
+		widget.NewButton("+", func() { f("+") }),
+		widget.NewButton("-", func() { f("-") }),
+	)
+	return c
 }
 
 func main() {
 	a := app.New()
+	w := a.NewWindow("Calc")
+	w.SetFixedSize(true)
 
-	w := a.NewWindow("Hello")
-	l := widget.NewLabel("Hello Fyne!")
+	l := widget.NewLabel("0")
+	l.Alignment = fyne.TextAlignTrailing
 
-	e := NewMyEntry(func(e *MyEntry) {
-		s := e.Text
-		e.SetText("")
-		l.SetText("you type '" + s + "'.")
-	})
+	data := cdata{
+		mem: 0,
+		cal: "",
+		flg: false,
+	}
+
+	calc := func(n int) {
+		switch data.cal {
+		case "":
+			data.mem = n
+		case "+":
+			data.mem += n
+		case "-":
+			data.mem -= n
+		case "*":
+			data.mem *= n
+		case "/":
+			data.mem /= n
+		}
+		l.SetText(strconv.Itoa(data.mem))
+		data.flg = true
+	}
+
+	pushNum := func(v int) {
+		s := l.Text
+		if data.flg {
+			s = "0"
+			data.flg = false
+		}
+		s += strconv.Itoa(v)
+		n, err := strconv.Atoi(s)
+		if err == nil {
+			l.SetText(strconv.Itoa(n))
+		}
+	}
+
+	pushCalc := func(c string) {
+		if c == "CL" {
+			l.SetText("0")
+			data.mem = 0
+			data.flg = false
+			data.cal = ""
+			return
+		}
+		n, err := strconv.Atoi(l.Text)
+		if err != nil {
+			return
+		}
+		calc(n)
+		data.cal = c
+	}
+
+	pushEnter := func() {
+		n, err := strconv.Atoi(l.Text)
+		if err != nil {
+			return
+		}
+		calc(n)
+		data.cal = ""
+	}
+
+	k := createNumButtons(pushNum)
+	c := createClacButtons(pushCalc)
+	e := widget.NewButton("Enter", pushEnter)
 
 	w.SetContent(
-		widget.NewVBox(l, e),
+		fyne.NewContainerWithLayout(
+			layout.NewBorderLayout(l, e, nil, c),
+			l,
+			e,
+			k,
+			c,
+		),
 	)
 
-	a.Settings().SetTheme(theme.LightTheme())
-	w.Resize(fyne.NewSize(300, 100))
+	w.Resize(fyne.NewSize(300, 200))
 	w.ShowAndRun()
 }
