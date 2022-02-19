e885802d9312733dbe1f512fa56eb21fb4b3abea
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2022-02-19 22:30:47 +0900

    ファイル情報を調べる

--- chapter.5/cmd/main.go
+++ chapter.5/cmd/main.go
@@ -1,32 +1,17 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
+	"io/ioutil"
 )
 
 func main() {
-	rt := func(f *os.File) {
-		r := bufio.NewReaderSize(f, 4096)
-		for i := 1; true; i++ {
-			s, _, err := r.ReadLine()
-			if err != nil {
-				break
-			}
-			fmt.Println(i, ": ", string(s))
-		}
-	}
-
-	fn := "data.txt"
-
-	f, err := os.OpenFile(fn, os.O_RDONLY, os.ModePerm)
+	fs, err := ioutil.ReadDir(".")
 	if err != nil {
 		panic(err)
 	}
-	defer f.Close()
 
-	fmt.Println("<<< start >>>")
-	rt(f)
-	fmt.Println("<<< end >>>")
+	for _, f := range fs {
+		fmt.Println(f.Name(), "(", f.Size(), ")")
+	}
 }
