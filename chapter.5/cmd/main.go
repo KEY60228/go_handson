69bc50111c42ecd223eefaffee4e88f8ffc5fc99
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2022-02-19 16:10:27 +0900

    ファイルを読み込む

--- chapter.5/cmd/main.go
+++ chapter.5/cmd/main.go
@@ -1,44 +1,29 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
+	"io/ioutil"
 	"os"
 )
 
 func main() {
-	wt := func(f *os.File, s string) {
-		_, err := f.WriteString(s + "\n")
+	rt := func(f *os.File) {
+		s, err := ioutil.ReadAll(f)
 		if err != nil {
 			panic(err)
 		}
+		fmt.Println(string(s))
 	}
+
 	fn := "data.txt"
 
-	f, err := os.OpenFile(fn, os.O_APPEND|os.O_CREATE|os.O_WRONLY, os.ModePerm)
+	f, err := os.OpenFile(fn, os.O_RDONLY, os.ModePerm)
 	if err != nil {
 		panic(err)
 	}
 	defer f.Close()
 
-	fmt.Println("*** start ***")
-	wt(f, "*** start ***")
-	for {
-		scanner := bufio.NewScanner(os.Stdin)
-		fmt.Print("type message: ")
-		scanner.Scan()
-		s := scanner.Text()
-
-		if s == "" {
-			break
-		}
-		wt(f, s)
-	}
-	wt(f, "*** end ***\n\n")
-	fmt.Println("*** end ***")
-
-	err = f.Close()
-	if err != nil {
-		fmt.Println(err)
-	}
+	fmt.Println("<<< start >>>")
+	rt(f)
+	fmt.Println("<<< end >>>")
 }
