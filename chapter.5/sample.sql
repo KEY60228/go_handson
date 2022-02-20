ecd27a02d68fb65a74e70d16ad4d15bb1c4d2639
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2022-02-20 09:52:35 +0900

    テーブルの準備

---
+++ chapter.5/sample.sql
@@ -0,0 +1,16 @@
+CREATE TABLE mydata (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    mail TEXT NOT NULL,
+    age INTEGER
+);
+
+INSERT INTO mydata (name, mail, age) VALUES (
+    'Taro', 'taro@yamada.com', 39
+), (
+    'Hanako', 'hanako@flower.com', 28
+), (
+    'Sachiko', 'sachiko@happy.com', 17
+), (
+    'Jiro', 'jiro@change.com', 6
+);
