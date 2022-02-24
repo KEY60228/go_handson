23a7612d8ab08347febc7261009858babb858383
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2022-02-24 22:33:44 +0900

    テンプレートでHTMLを出力する

--- chapter.6/cmd/main.go
+++ chapter.6/cmd/main.go
@@ -1,13 +1,29 @@
 package main
 
-import "net/http"
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
 
 func main() {
+	html := `<html><body>
+	<h1>Hello</h1>
+	<p>This is sample message.</p>
+	</body></html>`
+
+	tf, err := template.New("index").Parse(html)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	hh := func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello, This is Go-server!"))
+		err := tf.Execute(w, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 
 	http.HandleFunc("/hello", hh)
-
-	http.ListenAndServe("", nil)
+	http.ListenAndServe(":50510", nil)
 }
